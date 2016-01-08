f3e7825c52602b117b2ae88ef74a4730ee78a63a
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2016-01-08 03:18:34 -0500

    Import files needed to strip the Gentoo filesystem

---
+++ strip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "Syncing a temporary copy of the portage tree..."
+emerge-webrsync -q
+
+USE=internal-glib emerge -q1 pkgconfig
+emerge -Nq @world
+eselect news read --quiet
+etc-update -q --automode=-3
+
+CLEAN_DELAY=0 emerge -c
+rm -fr /usr/share/{doc,man} /usr/lib/python3.4/test /usr/portage /var/tmp/portage
